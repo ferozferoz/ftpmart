b39a493fafd7c5e86cb1ca1792cda95a8ee60171
ambreen nasir <feroz.nsit@gmail.com>
2021-10-07 13:37:56 +0800

    ver-10Oct2021

--- ftpmart/settings.py
+++ ftpmart/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS=[BASE_DIR / "static"]
+STATIC_ROOT= BASE_DIR / "static_cdb"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
